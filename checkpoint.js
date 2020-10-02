eec0f9bdce7598edc287b021f249f60b175eee94
unknown <roc666@protonmail.com>
2020-10-02 11:26:02 -0300

    CP commiteado por dbm4x a las 1601648762789

--- checkpoint.js
+++ checkpoint.js
@@ -253,6 +253,14 @@ function filtrar(funcion) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
 
+Array.prototype.filtrar = function(cb){
+  var h = [];
+	if(cb(this)) {
+		h.push(this);
+	}
+return h;
+}
+
 };
 
 // No modifiques nada debajo de esta linea
